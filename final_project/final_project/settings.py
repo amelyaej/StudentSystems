b214f6f63de7e5b0e23cf4bcd112f2b0c0548f95
amelyaej <148341120+amelyaej@users.noreply.github.com>
2025-05-18 21:22:53 +0700

    home page - amel

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -115,9 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'final_app/static'),
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
